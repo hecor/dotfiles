a5f3405a1d3c12594a511ffe5691294f3f819d93
hecor <husthecor@gmail.com>
2012-08-26 00:23:43 +0800

    update bin

--- bin/auto_install.sh
+++ bin/auto_install.sh
@@ -5,6 +5,10 @@ sudo apt-get install easystroke
 sudo apt-get install vim
 sudo apt-get install ctags
 
+# for install git
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libexpat-dev
+
 # for openschool
 sudo apt-get install apache2
 sudo apt-get install php5
@@ -13,3 +17,5 @@ sudo apt-get install php5-mysql
 sudo /etc/init.d/apache2 restart
 
 sudo apt-get install gettext
+
+
