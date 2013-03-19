f1d28950c50842c6970c72cbccbf2f4c13f324ff
hecor <husthecor@gmail.com>
2013-03-19 21:34:14 +0800

    Update auto_config.sh
    change bin to .bin

--- bin/auto_config.sh
+++ bin/auto_config.sh
@@ -10,15 +10,15 @@ fi
 if [ -f ~/.bash_aliases ]; then
 	mv ~/.bash_aliases /tmp/
 fi
-if [ -f ~/bin ]; then
-	mv ~/bin /tmp/
+if [ -f ~/.bin ]; then
+	mv ~/.bin /tmp/
 fi
 
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
 ln -s ~/dotfiles/bash/aliases ~/.bash_aliases
-ln -s ~/dotfiles/bin ~/bin
+ln -s ~/dotfiles/bin ~/.bin
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
