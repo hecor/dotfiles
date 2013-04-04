520082470066d21c9ca906dc6b096c155e18f66f
hecor <husthecor@gmail.com>
2013-04-04 22:26:59 +0800

    Update auto_config.sh

--- bin/auto_config.sh
+++ bin/auto_config.sh
@@ -13,12 +13,16 @@ fi
 if [ -f ~/.bin ]; then
 	mv ~/.bin /tmp/
 fi
+if [ -f ~/.screenrc ]; then
+	mv ~/.screenrc /tmp/
+fi
 
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
 ln -s ~/dotfiles/bash/aliases ~/.bash_aliases
 ln -s ~/dotfiles/bin ~/.bin
+ln -s ~/dotfiles/bash/screenrc ~/.screenrc
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
