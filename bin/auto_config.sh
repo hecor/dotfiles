20e5fe575f7ba865f5f4c534538e3fa6588890a3
hecor <husthecor@gmail.com>
2015-04-15 17:31:37 +0800

    change ln -s to cp in auto_config.sh

--- bin/auto_config.sh
+++ bin/auto_config.sh
@@ -10,19 +10,15 @@ fi
 if [ -f ~/.bash_aliases ]; then
 	mv ~/.bash_aliases /tmp/
 fi
-if [ -f ~/.bin ]; then
-	mv ~/.bin /tmp/
-fi
 if [ -f ~/.screenrc ]; then
 	mv ~/.screenrc /tmp/
 fi
 
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/bash/bashrc ~/.bashrc
-ln -s ~/dotfiles/bash/aliases ~/.bash_aliases
-ln -s ~/dotfiles/bin ~/.bin
-ln -s ~/dotfiles/bash/screenrc ~/.screenrc
+cp -r ~/dotfiles/vim ~/.vim
+cp ~/dotfiles/vim/vimrc ~/.vimrc
+cp ~/dotfiles/bash/bashrc ~/.bashrc
+cp ~/dotfiles/bash/aliases ~/.bash_aliases
+cp ~/dotfiles/bash/screenrc ~/.screenrc
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
